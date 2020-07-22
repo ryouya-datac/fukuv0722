16856cdad6015269599ada267d96789441d0c2eb
pizza_girl <pizza.girl.dat@gmail.com>
2020-07-22 13:33:58 +0900

    復習

--- fukuv0722/Form1.cs
+++ fukuv0722/Form1.cs
@@ -16,5 +16,41 @@ namespace fukuv0722
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Left += int.Parse(textBox1.Text);
+            label2.Top += int.Parse(textBox2.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Top = label2.Top - 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Top = label2.Top + 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Left = label2.Left + 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Left = label2.Left - 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
